8a9ddebb38ee40d50fcde4ba09e3745039252344
RafalSobieraj <rafal.sob@op.pl>
2022-09-06 01:56:28 +0200

    Test Application

--- src/main/java/com/example/demo/model/article/ArticleController.java
+++ src/main/java/com/example/demo/model/article/ArticleController.java
@@ -13,60 +13,69 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 
 
-@Controller
+@RestController
 public class ArticleController {
     
     @Autowired private ArticleService articleService;
+    ModelAndView modelAndView = new ModelAndView();
 
     @GetMapping("")
-    public String homePage(Model model){
+    public ModelAndView homePage(Model model){
         List<Article> articleList = articleService.articlesList();
         model.addAttribute("articleList", articleList);
 
-        return "index";
+        modelAndView.setViewName("index.html");
+        return modelAndView;
     }
 
     @GetMapping("/search")
-    public String search(@Param("keyword") String keyword, Model model){
+    public ModelAndView search(@Param("keyword") String keyword, Model model){
         List<Article> resultList = articleService.search(keyword);
         model.addAttribute("keyword", keyword);
         model.addAttribute("searchResult", "Znalezione elementy dla '" + keyword + "'");
         model.addAttribute("resultList", resultList);
 
-        return "result_search";
+        modelAndView.setViewName("result_search.html");
+        return modelAndView;
     }
 
     @GetMapping("/searchID")
-    public String searchByID(@Param("keywordID") Integer keywordID, Model model){
+    public ModelAndView searchByID(@Param("keywordID") Integer keywordID, Model model){
         List<Article> resultList = articleService.searchByID(keywordID);
         model.addAttribute("keyword", keywordID);
         model.addAttribute("searchResult", "Znalezione elementy dla ID = '" + keywordID + "'");
         model.addAttribute("resultList", resultList);
-
-        return "result_search";
+        
+        modelAndView.setViewName("result_search.html");
+        return modelAndView;
     }
 
     @GetMapping("/article/{id}")
-    public String selectArticle(@PathVariable("id") Integer id, Model model, RedirectAttributes re){
+    public ModelAndView selectArticle(@PathVariable("id") Integer id, Model model, RedirectAttributes re){
         try{
             Article article = articleService.get(id);
             model.addAttribute("articleContent", article.getContent());
             model.addAttribute("articleAuthor", article.getAuthor());
             model.addAttribute("articleTitle", article.getTitle());
-            return "article_select";
+
+            modelAndView.setViewName("article_select.html");
+            return modelAndView;
         } catch(NotFoundException a){
             re.addFlashAttribute("message", a.getMessage());
-            return "redirect:/";
+            modelAndView.setViewName("index.html");
+            return modelAndView;
         }
 
     }
     
     @GetMapping("/article/delete/{id}")
-    public String deleteArticle(@PathVariable("id") Integer id, RedirectAttributes re, Article article, Model model){
+    public ModelAndView deleteArticle(@PathVariable("id") Integer id, RedirectAttributes re, Article article, Model model){
 
         try {
             articleService.delete(id);
@@ -74,40 +83,48 @@ public class ArticleController {
         } catch (NotFoundException e) {
             re.addFlashAttribute("message", "Wystąpił błąd przy usuwaniu.");
         }
-        return "redirect:/";
+        modelAndView.setViewName("index.html");
+        return modelAndView;
     }
 
     @GetMapping("/article/new")
-    public String addNewArticle(Model model){
+    public ModelAndView addNewArticle(Model model){
         model.addAttribute("article", new Article());
         model.addAttribute("title", "Tworzenie nowego artykułu");
         Date date = new Date();
         Timestamp timestamp = new Timestamp(date.getTime());
         model.addAttribute("timestamp", timestamp);
-        return "article_form";
+
+        modelAndView.setViewName("article_form.html");
+        return modelAndView;
         }
 
     @PostMapping("/article/save")
-    public String articleSave(@ModelAttribute(name = "article") Article article,
+    public ModelAndView articleSave(@ModelAttribute(name = "article") Article article,
     RedirectAttributes re, Model model){
 
         articleService.save(article);
         re.addFlashAttribute("message", "Pomyślnie dodano artykuł");
 
-        return "redirect:/";
+        modelAndView.setViewName("index.html");
+        return modelAndView;
 
     }
 
     @GetMapping("/article/edit/{id}")
-    public String articleEdit(@PathVariable("id") Integer id, Model model, RedirectAttributes re){
+    public ModelAndView articleEdit(@PathVariable("id") Integer id, Model model, RedirectAttributes re){
         try {
             Article article = articleService.get(id);
             model.addAttribute("article", article);
             model.addAttribute("title", "Edycja artykułu o id " + id);
-            return "article_form";
+
+            modelAndView.setViewName("article_form.html");
+            return modelAndView;
         } catch (NotFoundException e) {
             re.addFlashAttribute("message", "Wystąpił błąd");
-            return "redirect:/";
+
+            modelAndView.setViewName("index.html");
+            return modelAndView;
         }
 
     }
